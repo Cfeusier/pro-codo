3160b51b6a52f2d132dc754892710d62c32f0a16
Clark Feusier <cfeusier@gmail.com>
2015-01-19 13:52:11 -0800

    Finish basic skeleton for app.js angular module

---
+++ client/app/app.js
@@ -0,0 +1,9 @@
+angular.module('Procodo', [
+  'Procodo.services',
+  'Procodo.users',
+  'Procodo.nav',
+  'ngRoute'
+])
+.config(function($routeProvider, $httpProvider) {
+
+});
---
+++ client/app/services/services.js
@@ -0,0 +1,1 @@
+angular.module('Procodo.services', []);
