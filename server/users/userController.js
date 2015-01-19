f4e8dfd5762e3d7aec736a9c31efe75f30688057
Clark Feusier <cfeusier@gmail.com>
2015-01-19 13:44:03 -0800

    Finish user controller, model, and route skeletons

---
+++ server/users/userController.js
@@ -0,0 +1,70 @@
+var User = require('./userModel.js');
+var Q = require('q');
+var jwt = require('jwt-simple');
+
+module.exports = {
+  login: function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var findUser = Q.nbind(User.findOne, User);
+
+    findUser({ username: username }).then(function (dev) {
+      if (!dev) {
+        next(new Error('User does not exist'));
+      } else {
+        return dev.comparePasswords(password).then(function(foundUser) {
+          if (foundUser) {
+            var token = jwt.encode(dev, 'secret');
+            res.json({ token: token });
+          } else {
+            return next(new Error('No User'));
+          }
+        });
+      }
+    }).fail(function (error) {
+      next(error);
+    });
+  },
+
+  signup: function (req, res, next) {
+    var username = req.body.username;
+    var password = req.body.password;
+    var create;
+    var newUser;
+    var findOne = Q.nbind(User.findOne, User);
+
+    findOne({ username: username }).then(function(dev) {
+      if (dev) {
+        next(new Error('User already exist!'));
+      } else {
+        create = Q.nbind(User.create, User);
+        newUser = {
+          username: username,
+          password: password
+        };
+        return create(newUser);
+      }
+    }).then(function (dev) {
+      var token = jwt.encode(dev, 'secret');
+      res.json({ token: token });
+    }).fail(function (error) {
+      next(error);
+    });
+  },
+
+  checkAuth: function (req, res, next) {
+    var token = req.headers['x-access-token'];
+
+    if (!token) {
+      next(new Error('No token'));
+    } else {
+      var dev = jwt.decode(token, 'secret');
+      var findUser = Q.nbind(User.findOne, User);
+      findUser({ username: user.username }).then(function (foundUser) {
+        foundUser ? res.send(200) : res.send(401);
+      }).fail(function (error) {
+        next(error);
+      });
+    }
+  }
+};
---
+++ server/users/userRoutes.js
@@ -0,0 +1,8 @@
+var userController = require('./userController.js');
+
+module.exports = function (app) {
+  // app === userRouter from middleware.js
+  app.post('/login', userController.login);
+  app.post('/signup', userController.signup);
+  app.get('/loggedin', userController.checkAuth);
+};
