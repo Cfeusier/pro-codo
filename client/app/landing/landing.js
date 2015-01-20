d774ef0cb6ae7bc9a5d9f5c6d5884ca194b874df
Clark Feusier <cfeusier@gmail.com>
2015-01-19 17:29:16 -0800

    Start welcome page for both user types

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -5,10 +5,16 @@ angular.module('Procodo.landing', [])
   $scope.data.welcome = '... loading ...';
 
   $scope.getUser = function () {
-    Users.getUser().then(function(user) {
-      $scope.data.user = user;
-      // if ()
-      $scope.data.welcome = "Clark";
+    Users.getUser().then(function(data) {
+      $scope.data.user = data.user;
+      console.log($scope.data.user)
+      if ($scope.data.user.uType == 1) {
+        $scope.data.welcome = 'Welcome Developer!';
+      } else if ($scope.data.user.uType === 2) {
+        $scope.data.welcome = 'Welcome Non-Profit!';
+      } else {
+        $scope.data.welcome = "Welcome!";
+      }
     }).catch(function(err) {
       console.error(err);
     });
