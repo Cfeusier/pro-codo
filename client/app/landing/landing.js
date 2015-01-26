495d9dd1d3ecf13bb0a61ca50ccad0b1586d4b98
Clark Feusier <cfeusier@gmail.com>
2015-01-26 12:12:10 -0800

    Remove unneccessary check in landing.js

--- client/app/landing/landing.js
+++ client/app/landing/landing.js
@@ -4,10 +4,12 @@ angular.module('Procodo.landing', [])
   $scope.welcome = '... loading ...';
 
   $scope.directUser = function () {
-    var type;
-    var userId = $window.localStorage.getItem('io.procodo.userId');
-    if (userId) {
-      type = $window.localStorage.getItem('io.procodo.uType');
+    Users.getUser(function(user) {
+      var type = user.uType;
+      $window.localStorage.setItem('io.procodo.userId', user._id);
+      $window.localStorage.setItem('io.procodo.username', user.username);
+      $window.localStorage.setItem('io.procodo.uType', user.uType);
+      $window.localStorage.setItem('io.procodo.profileId', user.profileId);
       if (type == 1) {
         $location.path('/devs/dashboard');
       } else if (type == 2) {
@@ -15,22 +17,7 @@ angular.module('Procodo.landing', [])
       } else {
         $location.path('/login');
       }
-    } else {
-      Users.getUser(function(user) {
-        type = user.uType;
-        $window.localStorage.setItem('io.procodo.userId', user._id);
-        $window.localStorage.setItem('io.procodo.username', user.username);
-        $window.localStorage.setItem('io.procodo.uType', user.uType);
-        $window.localStorage.setItem('io.procodo.profileId', user.profileId);
-        if (type == 1) {
-          $location.path('/devs/dashboard');
-        } else if (type == 2) {
-          $location.path('/nps/dashboard');
-        } else {
-          $location.path('/login');
-        }
-      });
-    }
+    });
   };
 
   $scope.name = 'LandingCtrl';
