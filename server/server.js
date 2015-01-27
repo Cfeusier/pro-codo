0b921e0da736176d1c06b6ad3bfe22253d47b7ff
Clark Feusier <cfeusier@gmail.com>
2015-01-26 19:40:55 -0800

    Add agnostic DB connection url for deployment

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
 
-// app server
 var app = express();
 
 // db setup
-var database = 'mongodb://localhost/procodo-dev';
-// if (process.env.NODE_ENV === 'production') {
-//   database = 'mongodb://'; // INSERT MONGOLAB URI
-// }
-mongoose.connect(database);
+var pEnv = process.env;
+var devDb = 'mongodb://localhost/procodo-dev';
+mongoose.connect(pEnv.MONGOLAB_URI || pEnv.MONGOHQ_URL  || devDb);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'mongo connection error:'));
 db.on('open', function() { console.log('mongo hooked'); });
