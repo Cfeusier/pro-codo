805384b1419aa0d7929d1b202f0d268cf730ba9b
Clark Feusier <cfeusier@gmail.com>
2015-01-26 19:45:25 -0800

    Remove unused helpers after refactor

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -7,19 +7,5 @@ module.exports = {
   },
   errorHandler: function(error, req, res, next) {
     res.status(500).send({ error: error.message });
-  },
-  decode: function(req, res, next) {
-    var token = req.headers['x-access-token'];
-    var user;
-
-    if (!token) return res.send(403);
-
-    try {
-      user = jwt.decode(token, 'secret');
-      req.user = user;
-      next();
-    } catch(error) {
-      return next(error);
-    }
   }
 };
