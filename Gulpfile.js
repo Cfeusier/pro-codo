56928d233981ab8a2a9feb8a725c3ddfa6d13209
Clark Feusier <cfeusier@gmail.com>
2015-02-01 14:11:36 -0800

    Change src and dest ref in gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,37 +9,37 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var bs = require('browser-sync');
 var reload = bs.reload;
+var src = './client/src';
+var dest = './client/public';
 
 var paths = {
-  src: './client/src',
-  dest: './client/public',
   scripts: ['client/app/**/*.js'],
   html: ['client/app/**/*.html', 'client/index.html'],
   styles: ['client/styles/style.css'],
   app: {
     src: [
-      this.src + '/app/users/users.js',
-      this.src + '/app/devs/devs.js',
-      this.src + '/app/projects/projects.js',
-      this.src + '/app/nps/nps.js',
-      this.src + '/app/nav/nav.js',
-      this.src + '/app/landing/landing.js',
-      this.src + '/app/services/lsKeys.js',
-      this.src + '/app/services/users.js',
-      this.src + '/app/services/devs.js',
-      this.src + '/app/services/nps.js',
-      this.src + '/app/services/projects.js',
-      this.src + '/app/services/project.js',
-      this.src + '/app/app.js'
+      src + '/app/users/users.js',
+      src + '/app/devs/devs.js',
+      src + '/app/projects/projects.js',
+      src + '/app/nps/nps.js',
+      src + '/app/nav/nav.js',
+      src + '/app/landing/landing.js',
+      src + '/app/services/lsKeys.js',
+      src + '/app/services/users.js',
+      src + '/app/services/devs.js',
+      src + '/app/services/nps.js',
+      src + '/app/services/projects.js',
+      src + '/app/services/project.js',
+      src + '/app/app.js'
     ],
-    dest: this.dest + '/app'
+    dest: dest + '/app'
   },
   vendor: {
     src: [
-      this.src + '/lib/angular/angular.js',
-      this.src + '/lib/angular-route/angular-route.js'
+      src + '/lib/angular/angular.js',
+      src + '/lib/angular-route/angular-route.js'
     ],
-    dest: this.dest + '/lib'
+    dest: dest + '/lib'
   }
 };
 
@@ -49,21 +49,21 @@ gulp.task('watch', function() {
 });
 
 gulp.task('appScripts', function() {
-  return gulp.src(paths.src)
+  return gulp.src(src)
     .pipe(concat('app.js'))
-    .pipe(gulp.dest(paths.dest))
+    .pipe(gulp.dest(dest))
     .pipe(rename('app.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.dest));
+    .pipe(gulp.dest(dest));
 });
 
 gulp.task('vendorScripts', function() {
-  return gulp.src(paths.src)
+  return gulp.src(src)
     .pipe(concat('vendor.js'))
-    .pipe(gulp.dest(paths.dest))
+    .pipe(gulp.dest(dest))
     .pipe(rename('vendor.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest(paths.dest));
+    .pipe(gulp.dest(dest));
 });
 
 gulp.task('build', ['appScripts', 'vendorScripts']);
